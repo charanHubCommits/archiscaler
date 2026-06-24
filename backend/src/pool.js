ffca2255a7cfef2b8f8aaac09c6028fa79d6ef28
charanHubCommits <charanmandakuriti@gmail.com>
2026-06-24 06:40:15 +0530

    add ssl for remote db connection

--- backend/src/pool.js
+++ backend/src/pool.js
@@ -5,7 +5,11 @@ const pool = new Pool({
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT 
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env.DB_CA,
+  },
 })
 
 module.exports = pool 
